257e2d6b1ac52e3d4f2899b1bd9c57b7a4b2d246
Atakan DULKER <contact@atkn.me>
2026-08-25 04:37:36 +0200

    fix(runtime): import object id payload type

--- Source/Runtime/RuntimeLifecyclePayload.swift
+++ Source/Runtime/RuntimeLifecyclePayload.swift
@@ -1,6 +1,7 @@
 // Copyright (c) 2026 Atakan DULKER. Licensed under the MIT License.
 
 import Foundation
+import AxolotyObjectModel
 import AxolotyWire
 
 /// Encodes the lifecycle payloads emitted by the host runtime.
